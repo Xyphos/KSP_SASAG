426a6f7dcc5b0e1da67de2cff45f9800ed433401
Xyphos <TheGreatXyphos@gmail.com>
2018-07-01 06:20:39 -0400

    minor code change

--- SASAG.cs
+++ SASAG.cs
@@ -36,52 +36,52 @@ namespace SASAG
         public void SetDisabled() => vessel.ActionGroups.SetGroup(group: KSPActionGroup.SAS, active: false);
 
         [KSPAction(guiName = "SAS Stability Assist")]
-        public void SetStabilityAssist(KSPActionParam param) => SetSAS(autopilotMode: VesselAutopilot.AutopilotMode.StabilityAssist);
+        public void SetStabilityAssist(KSPActionParam param) => SetSAS(mode: VesselAutopilot.AutopilotMode.StabilityAssist);
 
         [KSPAction(guiName = "SAS Maneuver Node")]
-        public void SetManeuverNode(KSPActionParam param) => SetSAS(autopilotMode: VesselAutopilot.AutopilotMode.Maneuver);
+        public void SetManeuverNode(KSPActionParam param) => SetSAS(mode: VesselAutopilot.AutopilotMode.Maneuver);
 
         [KSPAction(guiName = "SAS Prograde")]
-        public void SetPrograde(KSPActionParam param) => SetSAS(autopilotMode: VesselAutopilot.AutopilotMode.Prograde);
+        public void SetPrograde(KSPActionParam param) => SetSAS(mode: VesselAutopilot.AutopilotMode.Prograde);
 
         [KSPAction(guiName = "SAS Retrograde")]
-        public void SetRetrograde(KSPActionParam param) => SetSAS(autopilotMode: VesselAutopilot.AutopilotMode.Retrograde);
+        public void SetRetrograde(KSPActionParam param) => SetSAS(mode: VesselAutopilot.AutopilotMode.Retrograde);
 
         [KSPAction(guiName = "SAS Radial Out")]
-        public void SetRadialOut(KSPActionParam param) => SetSAS(autopilotMode: VesselAutopilot.AutopilotMode.RadialIn); // This is backwards in KSP???
+        public void SetRadialOut(KSPActionParam param) => SetSAS(mode: VesselAutopilot.AutopilotMode.RadialIn); // This is backwards in KSP???
 
         [KSPAction(guiName = "SAS Radial In")]
-        public void SetRadialIn(KSPActionParam param) => SetSAS(autopilotMode: VesselAutopilot.AutopilotMode.RadialOut); // This is backwards in KSP???
+        public void SetRadialIn(KSPActionParam param) => SetSAS(mode: VesselAutopilot.AutopilotMode.RadialOut); // This is backwards in KSP???
 
         [KSPAction(guiName = "SAS Normal")]
-        public void SetNormal(KSPActionParam param) => SetSAS(autopilotMode: VesselAutopilot.AutopilotMode.Normal);
+        public void SetNormal(KSPActionParam param) => SetSAS(mode: VesselAutopilot.AutopilotMode.Normal);
 
         [KSPAction(guiName = "SAS Anti-Normal")]
-        public void SetAntiNormal(KSPActionParam param) => SetSAS(autopilotMode: VesselAutopilot.AutopilotMode.Antinormal);
+        public void SetAntiNormal(KSPActionParam param) => SetSAS(mode: VesselAutopilot.AutopilotMode.Antinormal);
 
         [KSPAction(guiName = "SAS Target")]
-        public void SetTarget(KSPActionParam param) => SetSAS(autopilotMode: VesselAutopilot.AutopilotMode.Target);
+        public void SetTarget(KSPActionParam param) => SetSAS(mode: VesselAutopilot.AutopilotMode.Target);
 
         [KSPAction(guiName = "SAS Anti-Target")]
-        public void SetAntiTarget(KSPActionParam param) => SetSAS(autopilotMode: VesselAutopilot.AutopilotMode.AntiTarget);
+        public void SetAntiTarget(KSPActionParam param) => SetSAS(mode: VesselAutopilot.AutopilotMode.AntiTarget);
 
         // ReSharper disable once InconsistentNaming
-        private void SetSAS(VesselAutopilot.AutopilotMode autopilotMode)
+        private void SetSAS(VesselAutopilot.AutopilotMode mode)
         {
-            if (!vessel.Autopilot.CanSetMode(mode: autopilotMode))
-            {
-                ScreenMessages.PostScreenMessage(message: string.Format(format: "Vessel cannot set SAS to {0}", arg0: autopilotMode));
-                return;
-            }
-
             if (!vessel.Autopilot.SAS.CanEngageSAS())
             {
                 ScreenMessages.PostScreenMessage(message: "Vessel cannot engage SAS");
                 return;
             }
 
+            if (!vessel.Autopilot.CanSetMode(mode: mode))
+            {
+                ScreenMessages.PostScreenMessage(message: $"Vessel cannot set SAS to {mode}");
+                return;
+            }
+            
             vessel.ActionGroups.SetGroup(group: KSPActionGroup.SAS, active: true); // turn on SAS
-            vessel.Autopilot.Enable(mode: autopilotMode);                          // set SAS mode
+            vessel.Autopilot.Enable(mode: mode);                          // set SAS mode
         }
 
 
